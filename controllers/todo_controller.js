0555b7df489c180503473bdb84406ba5fadc307b
luginasp98 <luginasp98@gmail.com>
2023-10-21 23:37:24 +0700

    HW W11 ku belum sempurna hiks

---
+++ controllers/todo_controller.js
@@ -0,0 +1,25 @@
+const {todo} = require('../models')
+
+class TodoController {
+    static async getAll(req, res, next) {
+
+    }
+
+    static async getOne(req, res, next) {
+        
+    }
+
+    static async create(req, res, next) {
+        
+    }
+
+    static async update(req, res, next) {
+        
+    }
+
+    static async delete(req, res, next) {
+        
+    }
+}
+
+module.exports = TodoController
---
+++ seeders/20231021160033-add-items.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Todos', [
+    {
+      title: 'Memotong semangka',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Memasak telor',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Memangkas rambut',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Todos', null, {});
+  }
+};
